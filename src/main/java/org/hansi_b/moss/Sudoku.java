094dca5f86b431b349822f5a8d41771c32cc2421
cb <9629717+hansi-b@users.noreply.github.com>
2019-06-01 21:59:20 +0200

    added get, set, unset
    * along with argument checks and tests
    * renamed fields to cells (in accordance with Wikipedia)

--- src/main/java/org/hansi_b/moss/Sudoku.java
+++ src/main/java/org/hansi_b/moss/Sudoku.java
@@ -5,7 +5,7 @@ public class Sudoku {
 	private static final int DEFAULT_SIZE = 9;
 
 	private final int size;
-	private final int[] fields;
+	private final int[] cells;
 
 	public Sudoku() {
 		this(DEFAULT_SIZE);
@@ -17,7 +17,64 @@ public class Sudoku {
 			throw new IllegalArgumentException(
 					String.format("Sudoku cannot be initialised with a non-square size (got %d)", size));
 		this.size = size;
-		this.fields = new int[size * size];
+		this.cells = new int[size * size];
 	}
 
+	/**
+	 *
+	 * @param row the 1-based row in the Sudoku (i.e., maximum is equal to the
+	 *            Sudoku's size)
+	 * @param col the 1-based column in the Sudoku (i.e., the maximum allowed value
+	 *            is the Sudoku's size)
+	 * @return the cell's value, a number between zero and the Sudoku's size; zero
+	 *         indicates that the field is empty
+	 */
+	public int get(final int row, final int col) {
+		checkRow(row);
+		checkCol(col);
+		return cells[rowCol2Index(row, col)];
+	}
+
+	/**
+	 *
+	 * @param row      the 1-based row in the Sudoku (i.e., maximum is equal to the
+	 *                 Sudoku's size)
+	 * @param col      the 1-based column in the Sudoku (i.e., the maximum allowed
+	 *                 value is the Sudoku's size)
+	 * @param newValue a number between one and the Sudoku's size (inclusive)
+	 */
+	public void set(final int row, final int col, final int newValue) {
+		checkRow(row);
+		checkCol(col);
+		checkValue(newValue);
+		cells[rowCol2Index(row, col)] = newValue;
+	}
+
+	public void unset(final int row, final int col) {
+		checkRow(row);
+		checkCol(col);
+		cells[rowCol2Index(row, col)] = 0;
+	}
+
+	private int rowCol2Index(final int row, final int col) {
+		return size * (row - 1) + col - 1;
+	}
+
+	private void checkRow(final int row) {
+		checkArg(row, "Row");
+	}
+
+	private void checkCol(final int col) {
+		checkArg(col, "Column");
+	}
+
+	private void checkValue(final int val) {
+		checkArg(val, "Cell value");
+	}
+
+	private void checkArg(final int arg, final String label) {
+		if (arg < 1 || arg > size)
+			throw new IllegalArgumentException(
+					String.format("%s argument must be positive and at most %d (is %d)", label, size, arg));
+	}
 }
--- src/test/java/org/hansi_b/moss/SudokuTest.java
+++ src/test/java/org/hansi_b/moss/SudokuTest.java
@@ -1,5 +1,6 @@
 package org.hansi_b.moss;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.hansi_b.moss.Sudoku;
@@ -22,4 +23,69 @@ public class SudokuTest {
 				.isExactlyInstanceOf(IllegalArgumentException.class) //
 				.hasMessage("Sudoku cannot be initialised with a non-square size (got 5)");
 	}
+
+	@Test
+	public void testGetOnEmptyNew() {
+		final Sudoku su = new Sudoku();
+		assertEquals(0, su.get(1, 1));
+		assertEquals(0, su.get(5, 5));
+		assertEquals(0, su.get(9, 9));
+	}
+
+	@Test
+	public void testGetWithIllegalArgs() {
+		final Sudoku su = new Sudoku();
+		assertThatExceptionThrownBy(() -> su.get(0, 1)) //
+				.isExactlyInstanceOf(IllegalArgumentException.class) //
+				.hasMessage("Row argument must be positive and at most 9 (is 0)");
+		assertThatExceptionThrownBy(() -> su.get(1, 0)) //
+				.isExactlyInstanceOf(IllegalArgumentException.class) //
+				.hasMessage("Column argument must be positive and at most 9 (is 0)");
+		assertThatExceptionThrownBy(() -> su.get(10, 1)) //
+				.isExactlyInstanceOf(IllegalArgumentException.class) //
+				.hasMessage("Row argument must be positive and at most 9 (is 10)");
+		assertThatExceptionThrownBy(() -> su.get(1, 10)) //
+				.isExactlyInstanceOf(IllegalArgumentException.class) //
+				.hasMessage("Column argument must be positive and at most 9 (is 10)");
+	}
+
+	@Test
+	public void testSetAndGet() {
+		final Sudoku su = new Sudoku();
+		su.set(1, 2, 3);
+		assertEquals(3, su.get(1, 2));
+	}
+
+	@Test
+	public void testGetWithIllegalValueArgs() {
+		final Sudoku su = new Sudoku();
+		assertThatExceptionThrownBy(() -> {
+			su.set(1, 1, 0);
+			return null;
+		}).isExactlyInstanceOf(IllegalArgumentException.class) //
+				.hasMessage("Cell value argument must be positive and at most 9 (is 0)");
+		assertThatExceptionThrownBy(() -> {
+			su.set(1, 1, 10);
+			return null;
+		}).isExactlyInstanceOf(IllegalArgumentException.class) //
+				.hasMessage("Cell value argument must be positive and at most 9 (is 10)");
+	}
+
+	@Test
+	public void testUnset() {
+		final Sudoku su = new Sudoku();
+		su.set(1, 2, 3);
+		assertEquals(3, su.get(1, 2));
+		su.unset(1, 2);
+		assertEquals(0, su.get(1, 2));
+	}
+
+	@Test
+	public void testUnsetIsAllowedOnEmptyCell() {
+		final Sudoku su = new Sudoku();
+		assertEquals(0, su.get(1, 2));
+		su.unset(1, 2);
+		assertEquals(0, su.get(1, 2));
+	}
+
 }
