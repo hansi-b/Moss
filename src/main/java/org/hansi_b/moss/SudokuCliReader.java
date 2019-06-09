bcc645a47dc25b999ebff320cb565509fd7ee9ba
cb <9629717+hansi-b@users.noreply.github.com>
2019-06-09 20:40:12 +0200

    minimal runnable jar setup
    * Added (untested!) SudokuCliReader as main class.
      * Reads a Sudoku from stdin, prints out whether it is solved.
    * gradle define run class, stdin, and manifest main-class

---
+++ src/main/java/org/hansi_b/moss/SudokuCliReader.java
@@ -0,0 +1,74 @@
+package org.hansi_b.moss;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * Reads a Sudoku from the command line and just prints whether it is solved.
+ *
+ * The format is space-separated numbers for each row, where empty cells are
+ * indicated by a zero or
+ */
+public class SudokuCliReader {
+
+	private static void out(final String fmt, final Object... args) {
+		System.out.println(String.format(fmt, args));
+	}
+
+	private static void err(final String fmt, final Object... args) {
+		System.err.println(String.format(fmt, args));
+	}
+
+	private static Integer getCellVal(final String token, final int size) {
+		final Integer val = Integer.parseInt(token);
+		if (val < 0 || val > size)
+			throw new IllegalArgumentException(
+					String.format("Value must be between zero and %d, but is %d", size, val));
+		return val > 0 ? val : null;
+	}
+
+	private static List<Integer> readValues() {
+		final List<Integer> values = new ArrayList<>();
+		int size = -1;
+
+		try (Scanner stdin = new Scanner(System.in)) {
+			String line;
+			int lineCount = 0;
+			while (stdin.hasNextLine() && !((line = stdin.nextLine().trim())).isEmpty()) {
+
+				final String[] tokens = line.split("\\s");
+				if (size == -1) {
+					size = tokens.length;
+				} else if (tokens.length != size) {
+					err("Line contains %d values, expected %d (ignoring this line).", tokens.length, size);
+					continue;
+				}
+
+				try {
+					final List<Integer> rowValues = Arrays.stream(tokens).map(t -> getCellVal(t, tokens.length))
+							.collect(Collectors.toList());
+					values.addAll(rowValues);
+				} catch (final RuntimeException ex) {
+					err("Encountered parse error for %s (ignoring this line): %s", Arrays.toString(tokens), ex);
+				}
+
+				if (++lineCount == size)
+					break;
+			}
+		}
+		return values;
+	}
+
+	public static void main(final String[] args) {
+		out("Please enter your Sudoku, one row a line.");
+		out("Separate cell values by whitespace; empty cells should be represented by zero.");
+
+		final List<Integer> values = readValues();
+
+		final Sudoku su = Sudoku.create(values.toArray(new Integer[0]));
+		out("The sudoku %s solved.", su.isSolved() ? "_is_" : "is _not_");
+	}
+}
