0f2e25e0ce3c672f43b388c87a68ca9acb74f919
cb <9629717+hansi-b@users.noreply.github.com>
2019-06-22 21:16:16 +0200

    * added Cell.positionComparator and test

--- src/main/java/org/hansi_b/moss/Cell.java
+++ src/main/java/org/hansi_b/moss/Cell.java
@@ -1,6 +1,21 @@
 package org.hansi_b.moss;
 
+import java.util.Comparator;
+
+/**
+ * A single field in the Sudoku. Has a row and a column (at least in the square
+ * version). Does not maintain its value, but lets the Sudoku do that. It is
+ * itself actually immutable.
+ */
 public class Cell {
+
+	public final static Comparator<Cell> positionComparator = new Comparator<Cell>() {
+		@Override
+		public int compare(final Cell c1, final Cell c2) {
+			final int rowDiff = c1.rowIdx - c2.rowIdx;
+			return rowDiff != 0 ? rowDiff : c1.colIdx - c2.colIdx;
+		}
+	};
 
 	private final Sudoku sudoku;
 	private final int rowIdx;
