6c4dc82188053cf8a04b0b7c3ca0210b61d8f970
Hans Bering <cb@ludwig>
2020-02-23 16:46:38 +0100

    migrated NakedSingleTest to Spock

---
+++ src/test/groovy/org/hansi_b/moss/explain/NakedSingleTest.groovy
@@ -0,0 +1,93 @@
+package org.hansi_b.moss.explain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.hansi_b.moss.Sudoku;
+import org.hansi_b.moss.explain.Move.Strategy;
+import org.junit.Test;
+
+import static org.hansi_b.moss.explain.MoveAsserts.*;
+
+class NakedSingleTest extends spock.lang.Specification {
+
+	NakedSingle technique = new NakedSingle()
+
+	def testFindsTrivialSoleInRow() {
+
+		when:
+		Integer[] values =
+				[1, 0, 2, 4]+
+				[0, 0, 0, 0]+
+				[0, 0, 0, 0]+
+				[0, 0, 0, 0]
+
+		Sudoku su = Sudoku.filled(values);
+
+		then:
+		assert technique.findMoves(su) == [
+			move(su, Strategy.NakedSingle, 0, 1, 3)
+		]
+	}
+
+	def testFindsSingleSoleFromCombination() {
+
+		when:
+		Integer[] values =
+				[1, 0, 0, 0]+
+				[0, 0, 3, 0]+
+				[0, 2, 0, 0]+
+				[0, 0, 0, 0]
+
+		Sudoku su = Sudoku.filled(values);
+
+		then:
+		technique.findMoves(su) == [
+			move(su, Strategy.NakedSingle, 1, 1, 4)
+		]
+	}
+
+	def testFindsMultipleFromCombinations() {
+
+		when:
+		Integer[] values =
+				[1, 0, 0, 0]+
+				[0, 0, 3, 0]+
+				[4, 2, 0, 0]+
+				[0, 0, 0, 0]
+		Sudoku su = Sudoku.filled(values);
+
+		then:
+
+		technique.findMoves(su) as Set == [
+			move(su, Strategy.NakedSingle, 1, 0, 2),
+			move(su, Strategy.NakedSingle, 1, 1, 4),
+			move(su, Strategy.NakedSingle, 2, 2, 1),
+			move(su, Strategy.NakedSingle, 3, 0, 3)] as Set
+	}
+
+	def testFindBig() {
+
+		/*
+		 * from http://www.sudoku-space.de/sudoku-loesungstechniken/
+		 */
+		when:
+		Integer[] values =
+				[0, 0, 0, 0, 0, 1, 0, 0, 0]+
+				[0, 0, 0, 0, 0, 5, 0, 0, 0]+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0]+
+				//
+				[0, 0, 0, 8, 0, 0, 0, 0, 0]+
+				[0, 0, 0, 0, 2, 0, 0, 0, 0]+
+				[0, 4, 0, 6, 0, 0, 7, 0, 0]+
+				//
+				[0, 0, 0, 0, 0, 0, 0, 0, 0]+
+				[0, 0, 0, 0, 0, 0, 0, 0, 0]+
+				[0, 0, 0, 0, 0, 9, 0, 0, 0]
+
+		Sudoku su = Sudoku.filled(values);
+
+		then:
+		technique.findMoves(su) == [
+			move(su, Strategy.NakedSingle, 5, 5, 3)
+		];
+	}
+}
