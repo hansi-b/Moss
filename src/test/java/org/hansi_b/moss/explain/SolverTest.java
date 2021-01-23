7b4ffdd17b73c124b63e0677b83611daf102db5f
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2021-01-23 16:55:53 +0100

    added a couple of examples
    * one cannot be solved yet

--- src/test/java/org/hansi_b/moss/explain/SolverTest.java
+++ src/test/java/org/hansi_b/moss/explain/SolverTest.java
@@ -58,7 +58,7 @@ class SolverTest {
 	}
 
 	@Test
-	void testSehrSchwer() {
+	void test_Zeit_schwer_2019_06_08() {
 
 		/*
 		 * Die Zeit (sehr schwer, 8th June 2019)
@@ -81,9 +81,69 @@ class SolverTest {
 		assertTrue(filled.isSolved());
 	}
 
+	@Test
+	void test_Zeit_unknown_2019_11_09() {
+		final Integer[] values = { //
+				8, 0, 0, 0, 0, 0, 9, 0, 0, //
+				0, 0, 5, 6, 9, 0, 3, 2, 0, //
+				0, 4, 0, 0, 1, 0, 5, 7, 0, //
+				//
+				0, 9, 0, 0, 0, 0, 0, 6, 0, //
+				6, 0, 0, 1, 0, 0, 0, 8, 0, //
+				5, 0, 0, 0, 0, 2, 0, 0, 0, //
+				//
+				0, 0, 3, 4, 2, 0, 0, 0, 0, //
+				0, 8, 0, 0, 5, 1, 0, 0, 2, //
+				2, 0, 0, 0, 7, 0, 0, 4, 0, //
+		};
+		final Sudoku su = Sudoku.filled(values);
+		assertTrue(solver.solve(su).isSolved());
+	}
+
 	@Disabled("Needs a more advanced technique")
 	@Test
-	void testSehrSchwer02() {
+	void test_Zeit_unknown_2019_12_01() {
+		final Integer[] values = { //
+				0, 5, 3, 0, 0, 0, 4, 0, 0, //
+				1, 0, 0, 4, 6, 3, 0, 0, 0, //
+				0, 0, 6, 0, 0, 0, 0, 9, 0, //
+				//
+				0, 6, 0, 0, 0, 0, 0, 0, 7, //
+				0, 0, 0, 0, 8, 9, 6, 5, 1, //
+				0, 1, 8, 0, 0, 0, 0, 4, 0, //
+				//
+				6, 0, 0, 9, 7, 0, 3, 8, 0, //
+				0, 4, 7, 0, 0, 5, 0, 0, 2, //
+				0, 0, 9, 2, 0, 0, 0, 0, 0, //
+		};
+		final Sudoku su = Sudoku.filled(values);
+		final Sudoku filled = solver.solve(su);
+		System.out.println(new JavaArrayPrinter().draw(filled));
+		assertTrue(filled.isSolved());
+	}
+
+	@Test
+	void test_Zeit_unknown_2019_11_10() {
+		final Integer[] values = { //
+				5, 0, 4, 0, 6, 1, 0, 9, 0, //
+				3, 1, 8, 9, 2, 5, 0, 7, 0, //
+				0, 2, 0, 0, 7, 0, 1, 0, 0, //
+				//
+				9, 0, 0, 1, 0, 0, 7, 8, 0, //
+				0, 0, 0, 6, 0, 0, 0, 0, 4, //
+				0, 0, 0, 0, 0, 0, 0, 0, 1, //
+				//
+				0, 0, 0, 0, 5, 0, 0, 0, 3, //
+				0, 0, 0, 3, 0, 0, 0, 0, 9, //
+				0, 0, 0, 0, 4, 8, 5, 6, 0, //
+		};
+		final Sudoku su = Sudoku.filled(values);
+		assertTrue(solver.solve(su).isSolved());
+	}
+
+	@Disabled("Needs a more advanced technique")
+	@Test
+	void test_Zeit_schwer_2020_10_16() {
 		/*
 		 * Die Zeit (sehr schwer, 16th October 2019)
 		 */
