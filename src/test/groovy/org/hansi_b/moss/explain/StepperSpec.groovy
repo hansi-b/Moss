199d3f56a3f17fdf8bfd0c4f08f1d8c5899d8ed5
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-02-16 14:23:41 +0100

    added tests for Stepper

---
+++ src/test/groovy/org/hansi_b/moss/explain/StepperSpec.groovy
@@ -0,0 +1,44 @@
+package org.hansi_b.moss.explain;
+
+import java.util.stream.Stream
+
+import org.hansi_b.moss.Sudoku
+
+import spock.lang.Specification
+
+public class StepperSpec extends Specification {
+
+	Technique tec = Mock()
+	Sudoku su = Mock()
+
+	def "can find next move"() {
+
+		when:
+		Move move = Mock()
+
+		tec.findMoves(_, _) >>> [
+			Stream.of(move),
+			Stream.empty()
+		]
+
+		def stepper = new Stepper(su, tec)
+
+		then:
+		def found = stepper.hasNext()
+		move == stepper.next()
+		!stepper.hasNext()
+	}
+
+	def "throws exception on no next element"() {
+
+		given:
+		tec.findMoves(_, _) >> Stream.empty()
+		def stepper = new Stepper(su, tec)
+
+		when:
+		stepper.next()
+
+		then:
+		thrown NoSuchElementException
+	}
+}
