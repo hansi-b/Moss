1888256e7ea48e6d07a12929c8f4c14b267ee594
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-03-05 12:45:12 +0100

    added convenience newIllegalErr

--- src/main/java/org/hansi_b/moss/Errors.java
+++ src/main/java/org/hansi_b/moss/Errors.java
@@ -5,5 +5,8 @@ public class Errors {
 	public static IllegalArgumentException illegalArg(final String fmt, final Object... args) {
 		return new IllegalArgumentException(String.format(fmt, args));
 	}
-	
+
+	public static IllegalStateException illegalState(final String fmt, final Object... args) {
+		return new IllegalStateException(String.format(fmt, args));
+	}
 }
